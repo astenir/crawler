fcbb4cacc4ae2809e2299b783cb19db5b23d2232
astenir <astenir@outlook.com>
2024-12-09 18:44:46 +0800

    microservice

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"net/http"
 	"time"
+
+	pb "github.com/astenir/crawler/proto/greeter"
+	etcdReg "github.com/go-micro/plugins/v4/registry/etcd"
+	gs "github.com/go-micro/plugins/v4/server/grpc"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"go-micro.dev/v4"
+	"go-micro.dev/v4/registry"
+	"go-micro.dev/v4/server"
+	"google.golang.org/grpc"
 
 	"github.com/astenir/crawler/collect"
 	"github.com/astenir/crawler/engine"
@@ -10,6 +22,8 @@ import (
 	"github.com/astenir/crawler/proxy"
 	"github.com/astenir/crawler/storage"
 	"github.com/astenir/crawler/storage/sqlstorage"
+
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"golang.org/x/time/rate"
 )
@@ -17,43 +31,48 @@ import (
 func main() {
 
 	// log
-	// plugin, c := log.NewFilePlugin("./log.txt", zapcore.InfoLevel)
-	// defer c.Close()
 	plugin := log.NewStdoutPlugin(zapcore.DebugLevel)
 	logger := log.NewLogger(plugin)
 	logger.Info("log init end")
 
+	// set zap global logger
+	zap.ReplaceGlobals(logger)
+
 	// proxy
-	proxyURLs := []string{}
-	p, err := proxy.RoundRobinProxySwitcher(proxyURLs...)
-	if err != nil {
+	proxyURLs := []string{"http://47.122.65.254:8080", "http://183.247.152.98:53281", "http://8.130.34.44:8800"}
+	var p proxy.ProxyFunc
+	var err error
+
+	if p, err = proxy.RoundRobinProxySwitcher(proxyURLs...); err != nil {
 		logger.Error("RoundRobinProxySwitcher failed")
+		return
 	}
 
+	// fetcher
 	var f collect.Fetcher = &collect.BrowserFetch{
 		Timeout: 30000 * time.Millisecond,
 		Logger:  logger,
 		Proxy:   p,
 	}
 
+	// storage
 	var storage storage.Storage
-	storage, err = sqlstorage.New(
+
+	if storage, err = sqlstorage.New(
 		sqlstorage.WithSqlUrl("root:123456@tcp(127.0.0.1:3326)/crawler?charset=utf8"),
 		sqlstorage.WithLogger(logger.Named("sqlDB")),
 		sqlstorage.WithBatchCount(2),
-	)
-	if err != nil {
+	); err != nil {
 		logger.Error("create sqlstorage failed")
 		return
 	}
 
-	//2秒钟1个
+	// speed limiter
 	secondLimit := rate.NewLimiter(limiter.Per(1, 2*time.Second), 1)
-	//60秒20个
 	minuteLimit := rate.NewLimiter(limiter.Per(20, 1*time.Minute), 20)
 	multiLimiter := limiter.MultiLimiter(secondLimit, minuteLimit)
 
-	// url
+	// init tasks
 	var seeds = make([]*collect.Task, 0, 1000)
 	seeds = append(seeds, &collect.Task{
 		Property: collect.Property{
@@ -71,6 +90,47 @@ func main() {
 		engine.WithSeeds(seeds),
 		engine.WithScheduler(engine.NewSchedule()),
 	)
-	s.Run()
 
+	// worker start
+	go s.Run()
+
+	// start http proxy to GRPC
+	go HandleHTTP()
+	// start grpc server
+	reg := etcdReg.NewRegistry(
+		registry.Addrs(":2379"),
+	)
+	service := micro.NewService(
+		micro.Server(gs.NewServer(
+			server.Id("1"),
+		)),
+		micro.Address(":9090"),
+		micro.Registry(reg),
+		micro.Name("go.micro.server.worker"),
+	)
+	service.Init()
+	pb.RegisterGreeterHandler(service.Server(), new(Greeter))
+	if err := service.Run(); err != nil {
+		logger.Fatal("grpc server stop")
+	}
+}
+
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *pb.Request, rsp *pb.Response) error {
+	rsp.Greeting = "Hello " + req.Name
+	return nil
+}
+func HandleHTTP() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := pb.RegisterGreeterGwFromEndpoint(ctx, mux, "localhost:9090", opts)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	http.ListenAndServe(":8080", mux)
 }
